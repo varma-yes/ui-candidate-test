062764d806b0e74981e2452f454b688342d25064
varma-yes <varma@yesmail.io>
2016-01-11 16:59:53 -0600

    Update jsanddom.js
     addedd code for most of the functions in the jsaddom.js file

--- jsanddom.js
+++ jsanddom.js
@@ -1,46 +1,128 @@
-/********************************
-Unit Test Example
-********************************/
-module( "Example Unit Test" );
-test( "Add test", 2, function() {
-    // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
+     // Example unit test function
+     function divide( a, b ) {
+        // To see the test pass, uncomment the following line
+        return a / b;
+     }
 
-    // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
-});
-/********************************
-Please create your tests below...
-********************************/
-test( "ReverseString Test", 2, function() {
-    // Verify the method exists
-    equal( typeof reverseString, 'function', 'Must contain a reverseString function' ); 
+     // Write a function that takes a single argument (a string) and returns the string reversed.
+     function reverseString(str) {
+         return str.split("").reverse().join("");;
+     }
 
-    // Make sure the result from the reverseString function is valid
-    ok( reverseString( 'testing' ), 'gnitset', 'Expected gnitset as the result, the result was: ' + reverseString( 'testing' ) );
-});
+     // Write a function that takes an array of numbers and returns the minimum value
+     function findMinValue(values) {
+         return  values.sort()[0];
+     }
 
-test( "FindMinValue Test", 2, function() {
-    // Verify the method exists
-    equal( typeof findMinValue, 'function', 'Must contain a findMinValue function' ); 
+     // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
+     function findDistinctValues(values) {
+         values = values.filter(function(item, pos) {
+			return values.indexOf(item) == pos;
+		});
+		return values;
+     }
 
-    // Make sure the result from the reverseString function is valid
-    ok( findMinValue( [7,3,4,6] ), 3, 'Expected gnitset as the result, the result was: ' + findMinValue( [7,3,4,6]) );
-});
+     // Write a function that logs the numbers from 1 to 100 to the console.
+     // For multiples of three print "Fizz" instead of the number.
+     // For multiples of five print "Buzz".
+     // For numbers which are multiples of both three and five print "FizzBuzz".
+     function doFizzBuzz() {
+         var i;
+	for (i = 1; i < 100; i++) {
+	if(i%3 == 0 & i%5 == 0){
+		console.log('FizzBuzz');
+	}else if(i%3 == 0){
+	console.log('Fizz');
+	}else if(i%5 == 0){
+	console.log('Buzz');
+	}else{
+	console.log(i);
+	}
+	}
+     }
 
-test( "findDistinctValues Test", 2, function() {
-    // Verify the method exists
-    equal( typeof findDistinctValues, 'function', 'Must contain a findMinValue function' ); 
+     // You have a master array of strings, where each element is a fruit name.
+     // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
+     // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
+     // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
+     function removeFruits(fruits, fruitsToRemove) {
+         fruits = fruitsToRemove.filter( function( el ) {
+			return fruits.indexOf( el ) < 0;
+		});
+		return fruits;
+     }
 
-    // Make sure the result from the reverseString function is valid
-    ok( findDistinctValues( [1,2,1,2,1,2] ), [1,2], 'Expected gnitset as the result, the result was: ' + findDistinctValues( [1,2,1,2,1,2]) );
-});
+     // Write a function to push either a simple value or an array of values onto a specified array.
+     // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
+     // If toPush is a simple value, it should be pushed onto array as an element.
+     // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
+     function pushOntoArray(array, toPush) {
+			 if(toPush instanceof Array){
+				 array =array.concat(toPush)
+			 }else{
+				 array.push(toPush)
+			 }
+			 return array;
+     }
 
-test( "removeFruits Test", 2, function() {
-    // Verify the method exists
-    equal( typeof removeFruits, 'function', 'Must contain a removeFruits function' ); 
+     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
+     function splitListStrUsingComma(sourceStr) {
+         // FILL THIS IN
+     }
 
-    // Make sure the result from the reverseString function is valid
-    ok( removeFruits(['Apple','Banana'], ['Apple'] ), ['Banana'], 'Expected gnitset as the result, the result was: ' + removeFruits( ['Apple','Banana'], ['Apple'] ) );
-});
+     // Write a function that will take any number of arguments and return their sum
+     function sum() {
+         var args = [].slice.call(arguments);
+		var total=0;
+ 
+		for(var i in args){
+			total += args[i];
+		}
+	return total;
+	}
 
+     // Write a function that will return true if a specified string consists of only whitespace.
+     function isOnlyWhitespace(sourceStr) {
+		if(!(sourceStr.match(/^\s+$/) === null)) {
+			return true;
+		}
+     }
+
+     // write an example of a javascript closure
+
+     // define a json object that represents a collection of people.
+     // each person should have the following properties
+     // - first name
+     // - last name
+     // - city
+     // - state
+     // - zip
+     // - a collection of phone numbers (home, work, mobile)
+
+
+     // Create a javascript object (DataTable) with the following:
+     // A private columns property (string array)
+     // A private rows property (string array)
+     // A public method addRows that adds an item to the rows array
+     // A public method addColumns that adds an item to the columns array
+     // A public method getData that returns the a json object representation of the DataTable
+     // Note: the row object should be a hash of the column name and row item value
+     // Example:
+     // .addColumns('column1', 'column2', 'column3');
+     // .addRow('value1A', 'value1B', 'value1C');
+     // .addRow('value2A', 'value2B', 'value2C');
+
+     // within div1, programatically create a
+     // SELECT element (with multiple items) and a button.
+     // when the button is clicked write out the name and value of the selected item to the console.
+
+     // Write 5 different jQuery selectors to retrieve the
+     // sample anchor in the markup below.
+
+     // Programatically create an array with 5 items.  Create a list item for each item in the array
+     // and add the list items to the unordered list with an id of "list1".
+
+     // Use javascript to add a list of checkboxes and 2 links
+     // to the div with an id of "foobar"
+     // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
+     // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
